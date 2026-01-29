0e6c3187218ac7026c07ba1285b744044124dc88
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-29 22:21:50 +0530

    Update the app.js with unwanted implimentations

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
-
-
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
-import SplashScreen from './src/splash/SplashScreen';
 
 const App = () => {
     return (
         <SafeAreaProvider>
             <NavigationContainer>
-                <AppNavigator>
-                    <SplashScreen />
-                </AppNavigator> 
+                <AppNavigator />
             </NavigationContainer>
         </SafeAreaProvider>
     );
