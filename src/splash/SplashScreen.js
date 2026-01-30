8aec823a4079a8881af218921bf8a2c29ed4542a
Nadil-Dulran <it23161788@my.sliit.lk>
2026-01-30 14:35:59 +0530

    Develop method to use splash screen with time

--- src/splash/SplashScreen.js
+++ src/splash/SplashScreen.js
@@ -1,12 +1,17 @@
 
-import React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, Alert, TouchableOpacity } from 'react-native';
+import React, { useEffect } from 'react';
+import { View, Text } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 const Stack = createNativeStackNavigator();
 
-const SplashScreen = () => {
+const SplashScreen = ({ navigation }) => {
+    useEffect(() => {
+        setTimeout(() => {
+            navigation.replace('Search');
+        }, 2000);
+    }, []);
     return (
         <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
             <Text>SplashScreen</Text>
