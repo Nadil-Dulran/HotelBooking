853810251a82b80bcf8793f5a0a03b7795ed6d06
Nadil-Dulran <it23161788@my.sliit.lk>
2026-02-01 23:50:33 +0530

    Remove unnecessary page imports

--- App.js
+++ App.js
@@ -2,9 +2,6 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
-import SplashScreen from './src/splash/SplashScreen';
-import HotelCard from './src/components/HotelCard';
-import SearchScreen from './src/screens/SearchScreen';
 
 const App = () => {
     return (
