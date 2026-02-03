13a1d5ee1e9c88a9acd46be7055cd2d9d549930e
Nadil-Dulran <it23161788@my.sliit.lk>
2026-02-03 12:01:10 +0530

    Remove SplashScreen timeout feature cause it moved to the App.js

--- src/splash/SplashScreen.js
+++ src/splash/SplashScreen.js
@@ -7,11 +7,7 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 const Stack = createNativeStackNavigator();
 
 const SplashScreen = ({ navigation }) => {
-    useEffect(() => {
-        setTimeout(() => {
-            navigation.replace('Search');
-        }, 2000);
-    }, []);
+
     return (
         <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
             <Text>SplashScreen</Text>
