7f0abc30178105e881f70449c6b729b5c38c16ba
Nadil-Dulran <it23161788@my.sliit.lk>
2026-02-05 10:37:39 +0530

    Remove unnecessary imports

--- src/booking/BookingsScreen.js
+++ src/booking/BookingsScreen.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text, TextInput, FlatList, StyleSheet, Alert, TouchableOpacity, ActivityIndicator } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 const BookingsScreen = ({ route }) => {
