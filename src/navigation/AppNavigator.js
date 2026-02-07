c0343062d590c9ae83427a68ccec11494a85c825
Nadil-Dulran <it23161788@my.sliit.lk>
2026-02-07 22:48:53 +0530

    Uncomment the navigation

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,44 +1,44 @@
 
-// import React from 'react';
-// import { createNativeStackNavigator } from '@react-navigation/native-stack';
-// import SearchScreen from '../screens/SearchScreen';
-// import BookingDetailScreen from '../screens/BookingDetailScreen';
-// import SplashScreen from '../splash/SplashScreen';
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SearchScreen from '../screens/SearchScreen';
+import BookingDetailScreen from '../screens/BookingDetailScreen';
+import SplashScreen from '../splash/SplashScreen';
 
-// const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator();
 
-// const AppNavigator = () => {
-//     return (
+const AppNavigator = () => {
+    return (
         
-//         <Stack.Navigator
-//             initialRouteName="Splash"
-//             screenOptions={{
-//                 headerStyle: {
-//                     backgroundColor: '#007AFF',
-//                 },
-//                 headerTintColor: '#fff',
-//                 headerTitleStyle: {
-//                     fontWeight: 'bold',
-//                 },
-//             }}
-//         >
-//             <Stack.Screen
-//                 name="Splash"
-//                 component={SplashScreen}
-//                 options={{ headerShown: false }}
-//             />
-//             <Stack.Screen
-//                 name="Search"
-//                 component={SearchScreen}
-//                 options={{ title: 'Hotel Booking' }}
-//             />
-//             <Stack.Screen
-//                 name="BookingDetails"
-//                 component={BookingDetailScreen}
-//                 options={{ title: 'Booking Details' }}
-//             />
-//         </Stack.Navigator>
-//     );
-// };
+        <Stack.Navigator
+            initialRouteName="Splash"
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: '#007AFF',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+            }}
+        >
+            <Stack.Screen
+                name="Splash"
+                component={SplashScreen}
+                options={{ headerShown: false }}
+            />
+            <Stack.Screen
+                name="Search"
+                component={SearchScreen}
+                options={{ title: 'Hotel Booking' }}
+            />
+            <Stack.Screen
+                name="BookingDetails"
+                component={BookingDetailScreen}
+                options={{ title: 'Booking Details' }}
+            />
+        </Stack.Navigator>
+    );
+};
 
-// export default AppNavigator;
+export default AppNavigator;
