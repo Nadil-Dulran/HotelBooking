42685b329a7da03c949de13f3ecbcb6108361420
Nadil-Dulran <it23161788@my.sliit.lk>
2026-02-07 23:16:16 +0530

    Remove unnecessary imports

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import AppNavigator from './src/navigation/AppNavigator';
@@ -6,7 +6,7 @@ import AppNavigator from './src/navigation/AppNavigator';
 
 const App = () => {
 
-  
+
   return (
     <SafeAreaProvider>
       <NavigationContainer>
