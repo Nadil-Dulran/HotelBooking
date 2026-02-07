b4cadd08f3fa0992987cba680a7b0dd14b13cfb2
Nadil-Dulran <it23161788@my.sliit.lk>
2026-02-07 23:08:24 +0530

    Update AppNavigator with new screen and splash screen time out feature

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -1,42 +1,58 @@
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SearchScreen from '../screens/SearchScreen';
 import BookingDetailScreen from '../screens/BookingDetailScreen';
 import SplashScreen from '../splash/SplashScreen';
+import BookingsScreen from '../booking/BookingsScreen';
 
 const Stack = createNativeStackNavigator();
 
 const AppNavigator = () => {
+
+      const [showSplash, setShowSplash] = useState(true);
+    
+      useEffect(() => {
+        const timer = setTimeout(() => {
+          setShowSplash(false);
+        }, 2000);
+    
+        return () => clearTimeout(timer);
+      }, []);
+
+
     return (
         
         <Stack.Navigator
-            initialRouteName="Splash"
-            screenOptions={{
-                headerStyle: {
-                    backgroundColor: '#007AFF',
-                },
-                headerTintColor: '#fff',
-                headerTitleStyle: {
-                    fontWeight: 'bold',
-                },
-            }}
+          screenOptions={{
+            headerStyle: { backgroundColor: '#007AFF' },
+            headerTintColor: '#fff',
+          }}
         >
+          {showSplash ? (
             <Stack.Screen
-                name="Splash"
-                component={SplashScreen}
-                options={{ headerShown: false }}
+              name="Splash"
+              component={SplashScreen}
+              options={{ headerShown: false }}
             />
-            <Stack.Screen
+          ) : (
+            <>
+              <Stack.Screen
                 name="Search"
                 component={SearchScreen}
                 options={{ title: 'Hotel Booking' }}
-            />
-            <Stack.Screen
+              />
+              <Stack.Screen
                 name="BookingDetails"
                 component={BookingDetailScreen}
                 options={{ title: 'Booking Details' }}
-            />
+              />
+              <Stack.Screen
+                name="Bookings"
+                component={BookingsScreen}
+                options={{ title: 'Bookings' }}
+              />
+            </>
+          )}
         </Stack.Navigator>
     );
 };
