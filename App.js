eb26cebcad7da1816119178b63e530667dc2106a
Nadil-Dulran <it23161788@my.sliit.lk>
2026-02-07 23:13:56 +0530

    Update App.js with defualt structure

--- App.js
+++ App.js
@@ -1,64 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppNavigator from './src/navigation/AppNavigator';
 
-import SplashScreen from './src/splash/SplashScreen';
-import SearchScreen from './src/screens/SearchScreen';
-import BookingDetailScreen from './src/screens/BookingDetailScreen';
-import BookingsScreen from './src/booking/BookingsScreen';
-
-const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [showSplash, setShowSplash] = useState(true);
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setShowSplash(false);
-    }, 2000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
+  
   return (
     <SafeAreaProvider>
       <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerStyle: { backgroundColor: '#007AFF' },
-            headerTintColor: '#fff',
-          }}
-        >
-          {showSplash ? (
-            <Stack.Screen
-              name="Splash"
-              component={SplashScreen}
-              options={{ headerShown: false }}
-            />
-          ) : (
-            <>
-              <Stack.Screen
-                name="Search"
-                component={SearchScreen}
-                options={{ title: 'Hotel Booking' }}
-              />
-              <Stack.Screen
-                name="BookingDetails"
-                component={BookingDetailScreen}
-                options={{ title: 'Booking Details' }}
-              />
-              <Stack.Screen
-                name="Bookings"
-                component={BookingsScreen}
-                options={{ title: 'Bookings' }}
-              />
-            </>
-          )}
-        </Stack.Navigator>
+        <AppNavigator/>
       </NavigationContainer>
     </SafeAreaProvider>
   );
 };
+
 
 export default App;
